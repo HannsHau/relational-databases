a8ce9a483e41cef2ddee90ab88ccfe9d3e437830
HannsHau <bierkoenig@web.de>
2026-04-21 08:59:07 +0200

    Exercise 7

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -7,19 +7,20 @@ router.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const blog = await Blog.create({...req.body})
     return res.json(blog)
   } catch(error) {
-    return res.status(400).json({ error })
+    next(error)
+    //return res.status(400).json({ error })
   }
 })
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
   if (!req.blog) {
-    return res.status(404).end()
+    next({ name: 'NotFound', message: 'Blog not found'})
   }
   next()
 }
@@ -34,5 +35,21 @@ router.delete('/:id', blogFinder, async (req, res) => {
   await req.blog.destroy()
   res.status(204).end()
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.error('We got an Error in our middleware!', error.message, ':', error.name)
+
+  if (error.name === 'NotFound') {
+    return res.status(404).json({ error })
+  }
+
+  if (error.name === 'SequelizeValidationError') {
+    return res.status(400).json({error})
+  }
+
+  next(error)
+}
+
+router.use(errorHandler)
 
 module.exports = router
