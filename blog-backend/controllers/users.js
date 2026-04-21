de46b5bb95e68bf27d16419fb31edb34a5669d76
HannsHau <bierkoenig@web.de>
2026-04-21 12:13:20 +0200

    Exercise 8

---
+++ blog-backend/controllers/users.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+const userFinder = async (req, res, next) => {
+  console.log('simple log in userFinder')
+  req.user = await User.findOne({
+    where: { username: req.params.username },
+  })
+  if (!req.user) {
+    next({ name: 'NotFound', message: 'User not found' })
+  }
+  next()
+}
+
+router.put('/:username', userFinder, async (req, res) => {
+  console.log('simple log')
+  req.user.name = req.body.name
+  console.log('body: ', req.body)
+  await req.user.save()
+  res.json(req.user)
+})
+
+module.exports = router
