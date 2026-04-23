e2d6051650579ebfd46543434b5babf0add2d7a6
HannsHau <bierkoenig@web.de>
2026-04-23 08:42:50 +0200

    Exercise 23

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const { SECRET } = require('../util/config')
 const { Op } = require('sequelize')
 
+const { tokenExtractor } = require('../util/middleware')
 const { Blog, User } = require('../models')
 
 router.get('/', async (req, res) => {
@@ -29,19 +30,7 @@ router.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch {
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-  next()
-}
+
 
 router.post('/', tokenExtractor, async (req, res, next) => {
   try {
--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 
 const { Blog, User, UserBlogs } = require('../models')
 const { sequelize } = require('../util/db')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
   console.log('got in readinglists')
 
   try {
-    const { blogId, userId } = req.body;  
+    const { blogId, userId } = req.body
     console.log('with blodId: ', blogId, ' and userId: ', userId)
     console.log('maybe check if entries exists')
 
-    const newEntry = {userId, blogId}
+    const newEntry = { userId, blogId }
 
     console.log('newEntry: ', newEntry)
 
@@ -23,5 +24,25 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+const userBlogsFinder = async (req, res, next) => {
+  req.userBlog = await UserBlogs.findByPk(req.params.id)
+  if (!req.userBlog) {
+    next({ name: 'NotFound', message: 'UserBlog (readinglist) not found' })
+  }
+  next()
+}
+
+router.put('/:id', userBlogsFinder, tokenExtractor, async (req, res, next) => {
+  if (req.userBlog.dataValues.userId != req.decodedToken.id) {
+    next({
+      name: 'DifferentUser',
+      message: 'user is not owner, modification not possible',
+    })
+  } else {
+    req.userBlog.state = req.body.state
+    await req.userBlog.save()
+    res.json(req.userBlog)
+  }
+})
 
 module.exports = router
